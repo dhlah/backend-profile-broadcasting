461a2c47d2ac0a5af7a97d677aee8b01fdcfb0f0
dhlah <aminahnaf03@gmail.com>
2024-01-02 16:29:26 +0700

    PINISH PROJEK ANJAY

--- server.js
+++ server.js
@@ -1,68 +1,76 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 import prisma from "./lib/db.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const app = express()
+const app = express();
+const PORT = 3001;
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 app.get("/member", async (req, res) => {
   const data = await prisma.member.findMany({
-    select: { 
+    select: {
       slug: true,
       nama: true,
       kelas: true,
       image: true,
       gender: true,
       jabatan: true,
-      divisi: true, 
-    }
+      divisi: true,
+    },
+    orderBy: {
+      prioritas: 'asc',
+    },
   });
 
   if (!data) {
     return res.status(404).json({
-        status: 404, 
-        message: "Not Found" 
+      status: 404,
+      message: "Not Found",
     });
   }
-  return res.status(200).json({ 
+  return res.status(200).json({
     status: 200,
     message: "OK",
-    data: data 
-});
+    data: data,
+  });
 });
 
-app.get('/member/:slug', async (req, res) => {
-  const { slug } = req.params
+app.get("/member/:slug", async (req, res) => {
+  const { slug } = req.params;
 
   const memberData = await prisma.member.findUnique({
-    where: { slug: slug }
-  })
+    where: { slug: slug },
+  });
   if (memberData) {
     const memberDivisi = await prisma.divisi.findMany({
       where: { memberId: memberData.slug },
-      select: { divisi: true }
-    })
+      select: { divisi: true },
+    });
     return res.status(200).json({
       status: 200,
-    message: "OK",
-    data: {
-      slug: memberData.slug,
-      nama: memberData.nama,
-      kelas: memberData.kelas,
-      gender: memberData.gender,
-      jabatan: memberData.jabatan,
-      divisi: memberDivisi
-    }
-    })
-  }
+      message: "OK",
+      data: {
+        slug: memberData.slug,
+        nama: memberData.nama,
+        kelas: memberData.kelas,
+        gender: memberData.gender,
+        jabatan: memberData.jabatan,
+        divisi: memberDivisi,
+      },
+    });
+  } else {
+    return res.status(404).json({
+      status: 404,
+      message: "Not Found",
+    });
+  } 
+});
 
-})
-
-app.listen(3001, () => {
-    console.log('Server Berjalan Di port 3001')
-})
+app.listen(PORT, () => {
+  console.log("Server Api Berjalan di Port 3001");
+});
