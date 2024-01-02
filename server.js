3bd2eeb95a9a6f8e3863998dbfd06d0fe0c27e9e
dhlah <aminahnaf03@gmail.com>
2024-01-02 16:39:48 +0700

    aUPDATEEE#2

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3001;
 
+app.use((req, res, next) => {
+  const origin = req.get('referer');
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
+});
+
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
+
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
